a12d20de4be272c0cd714d04a55cd3a9621a929c
Ankit <Ankit@Ankit-PC>
2017-05-21 15:02:07 +0530

    Basheer anna chesina changes

--- GIT_Project1/src/package1/CLass1.java
+++ GIT_Project1/src/package1/CLass1.java
@@ -12,5 +12,7 @@ public class CLass1
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.testingmasters.com/hrm/symfony/web/index.php/auth/login");
 		driver.findElement(By.xpath("//input[@name='txtUsername']")).sendKeys("ankit03");
+		driver.findElement(By.name("txtPassword")).sendKeys("abcd1234");
+		driver.findElement(By.name("Submit")).click();
 	}
 }
