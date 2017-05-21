dcdaa0a99689cb2558626494b7b2657e6f704e1d
Ankit <Ankit@Ankit-PC>
2017-05-21 14:51:06 +0530

    Add by ankit first time

---
+++ GIT_Project1/src/package1/CLass1.java
@@ -0,0 +1,16 @@
+package package1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CLass1 
+{
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.testingmasters.com/hrm/symfony/web/index.php/auth/login");
+		driver.findElement(By.xpath("//input[@name='txtUsername']")).sendKeys("ankit03");
+	}
+}
